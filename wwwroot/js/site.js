31e7a05efc5b2f9f02dbf6cecc73a59db5b70294
Andres <cheche3064@gmail.com>
2020-04-23 14:28:17 -0500

    Agregando funcionalidad al boton enviar

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+//validaciones
+$('#search-button').on('click', function () {
+    var inputOption = $('#search-select option:selected').text();
+    var inputText = $('#search-text').val();
+    if (inputText == "") {
+        alert("¡Por favor ingresa al menos una palabra clave!");
+        return false;
+    } else {
+        var data = { "inputOption": inputOption, "inputText": inputText };
+        data = JSON.stringify(data);
+        $.post('../Libro/Index', data, function (resp) {
+            console.log(resp)
+        })
+        return true;
+    }
+});
